ce3def3a5f2f05d06912196167180a541c021ef6
kjstudy-englife <kjstudy.englife@gmail.com>
2024-01-09 08:49:10 +0900

    [WIP] Ch.12

---
+++ app/lib/actions.ts
@@ -0,0 +1,26 @@
+'use server';
+
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+
+const FromSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FromSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCentes = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+  // Test it out:
+  // console.log(rawFormData);
+}
